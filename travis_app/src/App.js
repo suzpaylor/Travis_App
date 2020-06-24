78b3d38001b30165d8edb3d4ea4d3ea738624844
Suzanne Paylor <38536047+suzpaylor@users.noreply.github.com>
2020-06-24 08:27:23 -0600

    new header

--- travis_app/src/App.js
+++ travis_app/src/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import './App.css';
 
+const style = {
+  margin: '0 auto',
+  width: '640px',
+};
 
+const title = <h1>My TODO App</h1>
 
 function App() {
   return (
-    <div className="App">
-      <h1>TRAVIS APP</h1>
+    <div style={style}>
+    <Title />
     </div>
   );
 }
+//components are JS functions that return JSX
+//function name has to be capitol for a component
 
 export default App;
+
+function Title(){
+  return <h1>My TODO APP Good job</h1>;
+}
